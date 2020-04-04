de6fe76825e1b2d09c948b91373a614c6fa0ed23
connisimmons <connisimmons@gmail.com>
2020-04-04 10:28:46 -0600

    cleaned

--- namecard.js
+++ namecard.js
@@ -1,6 +1,5 @@
-
-  function showInput() {
-    document.getElementById('display').innerHTML = 
-    document.getElementById("user_input").value;
-            }
+function showInput() {
+  document.getElementById('display').innerHTML = 
+  document.getElementById("user_input").value;
+    }
         
